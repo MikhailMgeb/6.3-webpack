963872cc0c725615d5a73a32b1132b938c8a569e
Michael Mgebrishvili <mmd920802@gmail.com>
2023-12-10 21:53:58 +0300

    image-minimizer-webpack-plugin

---
+++ src/js/api-key.js
@@ -0,0 +1,2 @@
+const API_KEY = 'luxfw7rqui299pwpwdflamw9baggz1zl55jm0dpxqjz1h3ym6';
+export default API_KEY;
---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+import API_KEY from './api-key';
+import '../css/index.css';
+
+const page = document.querySelector('.page');
+const image = document.createElement('img');
+
+fetch(
+  `https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=${API_KEY}`,
+)
+  .then((response) => response.json())
+  .then((word) => {
+    image.classList.add('theme');
+    return fetch(
+      `https://api.unsplash.com/search/photos?query=${word.word}&client_id=Sc_x-ousk_bVIWSlFcHp9sER1cCtnxwmZDd8quQnCyk`,
+    );
+  })
+  .then((response) => response.json())
+  .then((url) => {
+    if (url.total === 0) {
+      return fetch(
+        'https://api.unsplash.com/photos/random?client_id=Sc_x-ousk_bVIWSlFcHp9sER1cCtnxwmZDd8quQnCyk',
+      )
+        .then((response) => response.json());
+    }
+
+    return url.results[0];
+  })
+  .finally(() => {
+    const loader = document.querySelector('.lds-ring');
+    loader.classList.add('hidden');
+  })
+  .catch(() => { 'error!'; })
+  .then((url) => {
+    image.src = url.urls.full;
+    page.appendChild(image);
+  })
+  .catch(console.log('нет фотки!!!'));

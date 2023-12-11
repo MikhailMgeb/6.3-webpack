bf961e20992dd22f778be2b18df7356ce8515e9a
Michael Mgebrishvili <mmd920802@gmail.com>
2023-12-11 20:37:42 +0300

    minimizer plugin png

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const config = require('./webpack.config.js');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = merge(config, {
   mode: 'development',

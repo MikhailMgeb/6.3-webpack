dbf0f77793980c8e59cde678d8ef46dffd717c33
Michael Mgebrishvili <mmd920802@gmail.com>
2024-01-01 14:32:28 +0300

    pug plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,42 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PugPlugin = require('pug-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: {
+    index: './src/pug/index.pug',
+    // 'route/to/page': './src/pug/index.pug',
+  },
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
+      // {
+      //   test: /\.css$/i,
+      //   use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      // },
+      // {
+      //   test: /\.s[ac]ss$/i,
+      //   use: [
+      //     MiniCssExtractPlugin.loader,
+      //     'css-loader',
+      //     {
+      //       loader: 'sass-loader',
+      //       options: {
+      //         implementation: require('sass'),
+      //       },
+      //     },
+
+      //   ],
+      // },
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              implementation: require('sass'),
-            },
-          },
-          
-        ],
+        test: /\.(css|sass|scss)$/,
+        use: ['css-loader', 'sass-loader'],
       },
       {
         test: /\.(?:js|mjs|cjs)$/,
@@ -49,22 +58,27 @@ module.exports = {
         type: 'asset/resource',
         loader: 'svgo-loader',
       },
+      {
+        test: /.pug$/,
+        loader: PugPlugin.loader,
+      },
     ],
   },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.squooshMinify,
-        },
-      }),
-    ],
-  },
+  // optimization: {
+  //   minimize: true,
+  //   minimizer: [
+  //     new ImageMinimizerPlugin({
+  //       minimizer: {
+  //         implementation: ImageMinimizerPlugin.squooshMinify,
+  //       },
+  //     }),
+  //   ],
+  // },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new MiniCssExtractPlugin(),
+    // new HtmlWebpackPlugin({
+    //   template: './src/index.html',
+    // }),
+    new PugPlugin(),
+    // new MiniCssExtractPlugin(),
   ],
 };
